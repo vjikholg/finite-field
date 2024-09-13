3fac749f5060741b7e177720efbf2c5627f4ba7b
pleasedontlaugh <pupper13@student.ubc.ca>
2024-09-13 11:40:35 -0700

    test commit

---
+++ finitegroup.js
@@ -0,0 +1,15 @@
+class FiniteGroup {
+    constructor(generators) { 
+        // implement shreier-sims here - group action by 
+        
+        this.elems = new Set();  
+    }
+
+    assertClosed(){ 
+        this.elems.forEach()
+    }
+
+    groupMult(i, j ) {
+        return Math.multiply(i,j); 
+    } 
+}
