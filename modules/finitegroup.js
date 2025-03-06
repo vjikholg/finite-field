54652619b0c853bac66455be135ddce37d392a02
pleasedontlaugh <pupper13@student.ubc.ca>
2025-03-06 13:57:16 -0800

    reorganized files

---
+++ modules/finitegroup.js
@@ -0,0 +1,17 @@
+import {Matrix} from "math.js"
+class FiniteGroup {
+    constructor(generators) { 
+        // implement shreier-sims here - group action by conjugation basically 
+        stabPt; 
+
+        this.elems = new Set();  
+    }
+
+    assertClosed(){ 
+        this.elems.forEach()
+    }
+
+    groupMult(i, j ) {
+        return Math.multiply(i,j); 
+    } 
+}
