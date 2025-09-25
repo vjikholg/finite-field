7a8a2bee6891e85926e277ead3295720e62d8e39
vjikholg <yuvict1@outlook.com>
2025-09-25 15:35:31 -0700

    changed naming of ByteMatrix to match js naming conventions implemented submatrix, cofactor, determinant algorithms for ByteMatrix

--- modules/bytematrix.js
+++ modules/bytematrix.js
@@ -1,8 +1,6 @@
 import { FieldRegistry, GFp, Z, isDomain } from "./domains";
 import { cyrb53 } from "./helpers/hashcode";
 
-export const UNSAFE = false; 
-
 function byteWidth(word) { 
     if (word === 'u8') return 1; 
     if (word === 'u16') return 2; 
@@ -19,17 +17,17 @@ function makeView(buffer, word) {
     throw new Error(`${word} is an invalid/unknown word`); 
 }
 
-export class byteMatrix {
-    #rows; #cols; #domain; #buffer; #view; #key; #dirty;
+export class ByteMatrix {
+    #rows; #cols; #domain; #buffer; #view; #key; #dirty; #len;
     static #invCache = new Map(); // (k, v) = (key, matrix); 
     
     constructor({rows, cols, p, buffer}) { 
         this.#rows = rows, 
         this.#cols = cols || rows; 
         this.#domain = FieldRegistry.getField(p); 
-        const len = rows * cols || rows * rows; 
+        this.#len = rows * cols || rows * rows; 
         const bw = byteWidth(this.#domain.word)
-        this.#buffer = (buffer instanceof ArrayBuffer) ? buffer : new ArrayBuffer(len * bw); 
+        this.#buffer = (buffer instanceof ArrayBuffer) ? buffer : new ArrayBuffer(this.#len * bw); 
         this.#view = makeView(this.#buffer, this.#domain.word); 
         this.#key = null; 
         this.#dirty = true;
@@ -43,6 +41,10 @@ export class byteMatrix {
 
     index(i,j) {
         if (i < 0 || i >= this.#rows || j < 0 || j >= this.#cols) throw new Error(`Index out of range: ${i}, ${j}`); 
+        return i * this.#cols + j;
+    }
+
+    unsafeIndex(i,j) {
         return i * this.#cols + j;
     }
 
@@ -69,15 +71,15 @@ export class byteMatrix {
         return this.#key;
     }
 
-    mult(B) { 
-        if(! B instanceof byteMatrix) throw new Error(`B: ${typeof(B)} is not instance of byteMatrix`); 
-        if(this.#cols !== B.#rows) throw new Error(`Dimensional mismatch: A rows: ${this.#rows}, B cols: ${B.#cols}`); 
-        if(this.#domain.id !== B.#domain.id) throw new Error(`Domain mismatch: A: ${this.#domain.id}, B:${B.domain.id}`);
+    mult(mtx) { 
+        if(! B instanceof ByteMatrix) throw new Error(`B: ${typeof(B)} is not instance of byteMatrix`); 
+        if(this.#cols !== mtx.#rows) throw new Error(`Dimensional mismatch: A rows: ${this.#rows}, B cols: ${B.#cols}`); 
+        if(this.#domain.id !== mtx.#domain.id) throw new Error(`Domain mismatch: A: ${this.#domain.id}, B:${B.domain.id}`);
         if(this.#domain instanceof GFp && this.#domain.p !== B.#domain.p) throw new Error(`p-Domain mismatch: A:${this.domain.p},  B:${B.domain.p}`)
         
         const n  = this.#rows, m = this.#cols, k = B.#cols; 
-        const C = new Matrix2({rows: n, cols: k, p: this.#domain.p})
-        const A = this.#view, BB = B.#view, CC = C.#view
+        const out = new Matrix2({rows: n, cols: k, p: this.#domain.p})
+        const A = this.#view, mtxView = mtx.#view, outView = out.#view
 
         if (this.#domain instanceof GFp) { 
             const p = this.#domain.p;  
@@ -88,8 +90,8 @@ export class byteMatrix {
                     let s = 0; 
                     
                     for (let t = 0; t < m; t++) {
-                        s += this.#domain.representative((A[ia + t] * BB[t*k + j])); 
-                        CC[io + j] = s; 
+                        s += this.#domain.representative((A[ia + t] * mtxView[t*k + j])); 
+                        outView[io + j] = s; 
                     }
                 }
             }
@@ -98,8 +100,8 @@ export class byteMatrix {
                 for (let j = 0 ; j < k; j++) { 
                     let s = 0; 
                     for (let t = 0; t < m; t++) {
-                        s += A[ia + t] * BB[t*k + j]
-                        CC[io + j] = s; 
+                        s += A[ia + t] * mtxView[t*k + j]
+                        outView[io + j] = s; 
                     }
                 }
             }
@@ -107,4 +109,121 @@ export class byteMatrix {
         C.#dirty = true; C.#key = null; 
         return C;
    }
+
+    subMatrix(row, column) { // if elements belong in certain rows, certain columns, not taken. 
+        let sol = new ByteMatrix({rows: row-1, cols: column-1, p: this.#domain.p})
+
+        for (let i = 0, si = 0; i < this.rows; i++) {
+            if (i == row) continue; 
+            for (let j = 0, sj = 0; j < this.cols; j++) {
+                if (j == column) continue; 
+                sol.#view[sol.unsafeIndex(si, sj)] = this.#view[this.unsafeIndex(i,j)];
+                sj++;
+            }
+            si++;
+        }
+        // console.log(sol.contents);
+        return sol; 
+    }
+
+    transpose() {
+        if (this.#rows === 1 && this.#cols === 1) {
+            return this; 
+        }
+
+        const view = this.#view;
+
+        let sol = new ByteMatrix({rows: this.#rows, cols: this.#cols, p: this.#domain.p}); 
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) { 
+                sol.#view[sol.unsafeIndex(j,i)] = view[this.unsafeIndex(i,j)];
+            }
+        }
+        return sol; 
+    }
+
+    det() {
+        if (this instanceof GFp) return this.#detGFp(); 
+        if (this instanceof Z) return this.#detZ(); 
+        throw new Error(`Cannot take determinant from: ${typeof(this)}`);
+    }
+
+    #detZ() {
+        if (this.#rows !== this.#cols) throw new Error(`Non-square matrix!: ${this.#view}`)
+
+        const n = this.#len;
+        if (n === 1) return this.#view[0];
+        
+        const view = this.#view
+        if (n === 4) return (view[0]*view[3] - view[1]*view[2])
+
+        let sol = 0; 
+        for (let i = 0; i < this.#cols; i++) {
+            sol += view[i] * this.subMatrix(0, i).#detZ();
+        }
+        
+        return sol; 
+    }  
+
+    #detGFp() {
+        if (this.#rows !== this.#cols) throw new Error(`Non-square matrix!: ${this.#view}`)
+
+        const n = this.#len;
+        if (n === 1) return this.#view[0];
+        
+        const view = this.#view
+        if (n === 4) return this.domain.representative(view[0]*view[3] - view[1]*view[2])
+
+        let sol = 0; 
+        for (let i = 0; i < this.#cols; i++) {
+            sol += view[i] * this.subMatrix(0, i).#detZ();
+        }
+        
+        return this.domain.representative(sol); 
+    }
+
+    cofactor() { 
+        if (this.#rows !== this.cols) throw new Error(`Non square matrix: ${this.#view}`);
+        if (this.#len === 1) return this; 
+        
+        const view = this.#view;
+        let sol = new ByteMatrix({rows: this.#rows, cols: this.#cols, p})
+        for (let i = 0 ; i < sol.#rows; i++ ) {
+            for (let j = 0; j < sol.#cols; j++) { 
+                sol.#view[this.unsafeIndex(i,j)] = this.#domain.representative(view[this.subMatrix(i,j).det() * ((-1) ** (i+j))]);
+            }
+        }
+
+        return sol; 
+
+    }
+
+    adjugate() { 
+        return this.cofactor().transpose();
+    }
+
+    inv() { 
+        if (this.#rows !== this.cols) throw new Error(`Non square matrix: ${this.#view}`);
+        const det = this.det(); 
+        if (this instanceof Z && Math.abs(det) !== 1) throw new Error(`Z-matrix non-invertible: det ${det}`);
+        const adj = this.adj(); 
+        const invdet = (this instanceof Z ?  parseFloat(1/parseFloat(det)) : this.#domain.invert(det));
+
+        let sol = new ByteMatrix({rows: this.#rows, cols: this.#cols, p: this.#domain.p})
+        const view = adj.#view
+        if (this instanceof Z) { 
+            for (let i = 0; i < this.#len; i++) {
+                sol[i] = view[i] * invdet; 
+            }
+        }
+
+        if (this instanceof Z) {
+            for (let i = 0; i < this.#len; i++) {
+                sol[i] = this.#domain.representative(view[i] * invdet); 
+            }
+        }
+        
+        return sol; 
+    }
 }
